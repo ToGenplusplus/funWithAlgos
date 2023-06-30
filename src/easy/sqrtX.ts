8709d8af9a60712d91ea7b83e55b58b769cfe41e
temiowoaje <temiowoaje@carfax.com>
2023-06-30 18:38:43 -0400

    added new problem sqrtX, noted down thought process

---
+++ src/easy/sqrtX.ts
@@ -0,0 +1,26 @@
+/**
+ * Given:
+ * - X - a non negative integer
+ *
+ * Return: s where s represents the square root of X rounded down to the nearest integer.
+ *
+ * Ex:
+ * X | S
+ * 4 | 2
+ * 16 | 4
+ * 8 | 3
+ * 11 | 3
+ * 44 | 6
+ * 142 | 11
+ *
+ * if X is 0 or 1 we can return X
+ * now the square root of X will be between the sqaure of two whole numbers so we just need to find the
+ * whole numbers whoses squares its between and choose the smallest whole number
+ *
+ * Ex X = 23:
+ * Iter | a | b | a2 | b2|
+ * 0 | 1 | 2| 1 | 4 |
+ * 1 | 2 | 3 | 4 | 9 |
+ * 2 | 3 | 4| 9 | 16
+ * 3 | 4 | 5 | 16 | 25 -> yes 23 is between 16 and 25 so the answer is 4
+ */
