d958e77102cb474e71d06c9dd5315d24d67aaa96
temiowoaje <temiowoaje@carfax.com>
2023-07-02 10:38:37 -0400

    coded working solution for climb stairs

--- src/easy/ClimbingStairs.ts
+++ src/easy/ClimbingStairs.ts
@@ -14,3 +14,30 @@
  *
  * Looks like the number of ways to reach n steps is = to # of ways to reach n- 1 steps + # of ways to reach n - 2 steps for n > 2.
  */
+
+/**
+ * Time complexity is O(n)
+ * Space complexity is O(n)
+ */
+
+let cache = new Object();
+cache[1] = 1;
+cache[2] = 2;
+cache[3] = 3;
+
+export function climbStairs(n: number): number {
+  if (n < 4) return n;
+
+  let previous;
+  let doublePrevious;
+  if (!cache.hasOwnProperty(n - 1)) {
+    cache[n - 1] = climbStairs(n - 1);
+  }
+
+  if (!cache.hasOwnProperty(n - 2)) {
+    cache[n - 2] = climbStairs(n - 2);
+  }
+  previous = cache[n - 1];
+  doublePrevious = cache[n - 2];
+  return previous + doublePrevious;
+}
